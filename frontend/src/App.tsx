cb925da5d1937e1fd890402bdf14edf43919d3be
Ramiro Rosales <ramirosales52@gmail.com>
2026-06-25 22:43:33 -0300

    Ocultar botón de WhatsApp en /admin

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -74,7 +74,7 @@ export default function App() {
         </Route>
       </Routes>
       {isProducts && <GoToTopButton />}
-      <WhatsAppButton />
+      {!pathname.startsWith('/admin') && <WhatsAppButton />}
     </div>
   );
 }
